9469f73c9472f093824327be9d4da8b77c0fd722
Angel Concepcion <angelconcepcion50@gmail.com>
2022-08-15 14:09:20 -0400

    solution for lab 3

--- src/index.js
+++
@@ -1,8 +0,0 @@
-function prepend(numSpaces, inputString) {
-    // your code here
-    for (var i = 0; i < numSpaces; i++) {
-        inputString = ' ' + inputString;
-    }
-    return inputString;
-}
-console.log(prepend(5, 'hi'));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-function prepend(numSpaces: number, inputString: string) {
+function prepend(numSpaces: number | string, inputString: string) {
 	// your code here
 	for (let i = 0; i < numSpaces; i++) {
 		inputString = ' ' + inputString;
@@ -7,3 +7,17 @@ function prepend(numSpaces: number, inputString: string) {
 }
 
 console.log(prepend(5, 'hi'));
+
+const button = document.querySelector("button") as HTMLButtonElement;
+const input1 = document.getElementById("firstNumber") as HTMLInputElement;
+const input2 = document.getElementById("firstString") as HTMLInputElement;
+
+button!.addEventListener("click", function () {
+  console.log(prepend(input1!.value, input2!.value));
+});
+
+
+// function add(firstNumber: any, secondNumber: any) {
+//   return firstNumber + secondNumber;
+// }
+// console.log(add(10, 20));
